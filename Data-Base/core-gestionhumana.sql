3610ff73726408a1ae80701b9b0b45be3c9b53be
jhta <jeisonhs93@gmail.com>
2014-05-07 15:39:23 -0500

    base full

--- Data-Base/core-gestionhumana.sql
+++ Data-Base/core-gestionhumana.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50536
 File Encoding         : 65001
 
-Date: 2014-05-07 14:57:26
+Date: 2014-05-07 15:38:48
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -41,7 +41,7 @@ DROP TABLE IF EXISTS `comentario`;
 CREATE TABLE `comentario` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `comentario` varchar(2000) COLLATE utf8_unicode_ci NOT NULL,
+  `comentario` text COLLATE utf8_unicode_ci NOT NULL,
   `PUBLICACION_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `PUBLICACION_id` (`PUBLICACION_id`),
@@ -57,7 +57,7 @@ CREATE TABLE `comentario` (
 -- ----------------------------
 DROP TABLE IF EXISTS `etiqueta`;
 CREATE TABLE `etiqueta` (
-  `nombre` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`nombre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -71,8 +71,8 @@ CREATE TABLE `etiqueta` (
 DROP TABLE IF EXISTS `publicacion`;
 CREATE TABLE `publicacion` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titulo` varchar(50) CHARACTER SET latin1 NOT NULL,
-  `contenido` varchar(5000) CHARACTER SET latin1 NOT NULL,
+  `titulo` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `contenido` text COLLATE utf8_unicode_ci NOT NULL,
   `USUARIO_id` int(11) NOT NULL,
   `fecha` date NOT NULL,
   PRIMARY KEY (`id`),
@@ -90,11 +90,11 @@ CREATE TABLE `publicacion` (
 DROP TABLE IF EXISTS `trending`;
 CREATE TABLE `trending` (
   `PUBLICACION_id` int(11) NOT NULL,
-  `ETIQUETA_nombre` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `ETIQUETA_nombre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`ETIQUETA_nombre`),
   KEY `publi` (`PUBLICACION_id`),
-  CONSTRAINT `eti` FOREIGN KEY (`ETIQUETA_nombre`) REFERENCES `etiqueta` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `publi` FOREIGN KEY (`PUBLICACION_id`) REFERENCES `publicacion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `publi` FOREIGN KEY (`PUBLICACION_id`) REFERENCES `publicacion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `eti` FOREIGN KEY (`ETIQUETA_nombre`) REFERENCES `etiqueta` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
@@ -107,12 +107,12 @@ CREATE TABLE `trending` (
 DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) CHARACTER SET latin1 NOT NULL,
-  `contraseña` varchar(25) CHARACTER SET latin1 NOT NULL,
-  `sesion` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
-  `correo` varchar(60) CHARACTER SET latin1 NOT NULL,
-  `descripcion` varchar(120) CHARACTER SET latin1 DEFAULT NULL,
-  `titulo` varchar(40) CHARACTER SET latin1 DEFAULT NULL,
+  `nombre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `contraseña` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `sesion` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `correo` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `descripcion` varchar(120) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `titulo` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
