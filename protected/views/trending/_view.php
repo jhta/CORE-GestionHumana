84fa2e73ceefda3e22de4be537a66324060d4600
CristianCamilo <CristianCamilo@172.32.3.115>
2014-05-07 21:06:28 -0500

    Vistas de los elementos de trending

--- protected/views/trending/_view.php
+++ protected/views/trending/_view.php
@@ -6,12 +6,15 @@
 <div class="view">
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('ETIQUETA_nombre')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->ETIQUETA_nombre), array('view', 'id'=>$data->ETIQUETA_nombre)); ?>
+	<?php echo CHtml::encode($data->ETIQUETA_nombre); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('PUBLICACION_id')); ?>:</b>
 	<?php echo CHtml::encode($data->PUBLICACION_id); ?>
 	<br />
+        <b>
+        <?php echo CHtml::link(CHtml::encode('ver'), array('view', 'PUBLICACION_id'=>$data->PUBLICACION_id,'ETIQUETA_nombre'=>$data->ETIQUETA_nombre)); ?>
+        </b>
 
 
 </div>
