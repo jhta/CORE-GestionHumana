06ea5b33778f9df375fb596636cd9e885af41887
jhta <jeisonhs93@gmail.com>
2014-05-08 22:12:30 -0500

    prueba

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -11,7 +11,7 @@ class SiteController extends Controller
 			// captcha action renders the CAPTCHA image displayed on the contact page
 			'captcha'=>array(
 				'class'=>'CCaptchaAction',
-				'backColor'=>0xFFFFFF,
+				'backColor'=>0xF2E3D4,
 			),
 			// page action renders "static" pages stored under 'protected/views/site/pages'
 			// They can be accessed via: index.php?r=site/page&view=FileName
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -3,8 +3,8 @@
 
 $this->pageTitle=Yii::app()->name;
 ?>
-
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<script>alert("hola");</script>
+<h1>Hola <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
 <p>Congratulations! You have successfully created your Yii application.</p>
 
