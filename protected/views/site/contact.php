cf223e3d1d22ceb3a54cea5c2827fa59c39fabde
jhta <jeisonhs93@gmail.com>
2014-05-15 17:26:19 -0500

    formulario conacto melo

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -9,8 +9,47 @@ $this->breadcrumbs=array(
 );
 ?>
 
-<h1>Contactanos</h1>
+<script>
 
+var validator = new FormValidator('contact-form', [{
+    name: 'req',
+    display: 'required',    
+    rules: 'required'
+}, {
+    name: 'alphanumeric',
+    rules: 'alpha_numeric'
+}, {
+    name: 'password',
+    rules: 'required'
+}, {
+    name: 'password_confirm',
+    display: 'password confirmation',
+    rules: 'required|matches[password]'
+}, {
+    name: 'email',
+    rules: 'valid_email',
+    depends: function() {
+        return Math.random() > .5;
+    }
+}, {
+    name: 'minlength',
+    display: 'min length',
+    rules: 'min_length[8]'
+}], function(errors, event) {
+    if (errors.length > 0) {
+         var errorString = '';
+        
+        for (var i = 0, errorLength = errors.length; i < errorLength; i++) {
+            errorString += errors[i].message + '<br />';
+        }
+        
+        $("#errorReport").append(errorString);
+    }
+});
+</script>
+
+
+<div id="errorReport"></div>
 <?php if(Yii::app()->user->hasFlash('contact')): ?>
 
 <div class="flash-success">
@@ -20,59 +59,109 @@ $this->breadcrumbs=array(
 <?php else: ?>
 
 
-<div class="form">
+<div class="contact-form" >
 
+<!--        	<form id="contact-form" class="contact-form" action="#">
+            	<p class="contact-name">
+            		<input id="contact_name" type="text" placeholder="Full Name" value="" name="name" />
+                </p>
+                <p class="contact-email">
+                	<input id="contact_email" type="text" placeholder="Email Address" value="" name="email" />
+                </p>
+                <p class="contact-message">
+                	<textarea id="contact_message" placeholder="Your Message" name="message" rows="15" cols="40"></textarea>
+                </p>
+                <p class="contact-submit">
+                	<a id="contact-submit" class="submit" href="#">Send Your Email</a>
+                </p>
+                
+                <div id="response">
+                
+                </div>
+            </form>-->
+    
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'contact-form',
+        
 	'enableClientValidation'=>true,
 	'clientOptions'=>array(
 		'validateOnSubmit'=>true,
+            
 	),
+        'htmlOptions'=>array(
+            'name'=>'contact-form'
+        ),
 )); ?>
 
 	<p class="note">Campos con <span class="required">*</span>, son "Campo Requerido"</p>
 
 	<?php echo $form->errorSummary($model); ?>
-
+        <p class="contact-name">
+            <div class="row">
+                    <?php //echo $form->labelEx($model,'name'); ?>
+                    <?php echo $form->textField($model,'name',
+                            array('required'=>true,
+                                'id'=>'contact_name',
+                                'placeholder'=>'Nombre Completo',
+                                'name'=>'name' )); ?>
+                    <?php echo $form->error($model,'name'); ?>
+            </div>
+        </p>
 	<div class="row">
-		<?php echo $form->labelEx($model,'name'); ?>
-		<?php echo $form->textField($model,'name'); ?>
-		<?php echo $form->error($model,'name'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'email'); ?>
-		<?php echo $form->textField($model,'email'); ?>
+		<?php echo $form->emailField($model,'email',
+                        array('required'=>true,
+                                'id'=>'contact_email',
+                                'placeholder'=>'Correo',
+                                'name'=>'email')); ?>
+                        
 		<?php echo $form->error($model,'email'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'subject'); ?>
-		<?php echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'subject'); ?>
+		
+		<?php echo $form->textField($model,'subject',
+                        array('size'=>60,
+                            'maxlength'=>128,
+                            'required'=>true,
+                            'id'=>'contact_subject',
+                            'placeholder'=>'Asunto',
+                            'name'=>'message'
+                            )
+                            
+                        ); ?>
+		
+                    <?php echo $form->error($model,'subject'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'body'); ?>
-		<?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>50)); ?>
+		
+		<?php echo $form->textArea($model,'body',array('rows'=>9,
+                    'cols'=>40,
+                    'required'=>true,
+                    'id'=>'contact_message',
+                    'placeholder'=>'Escribe aqui tu mensaje',
+                    'name'=>'message'
+                    )); ?>
 		<?php echo $form->error($model,'body'); ?>
 	</div>
 
-	<?php if(CCaptcha::checkRequirements()): ?>
-	<div class="row">
-		<?php echo $form->labelEx($model,'verifyCode'); ?>
-		<div>
-		<?php $this->widget('CCaptcha'); ?>
-		<?php echo $form->textField($model,'verifyCode'); ?>
-		</div>
-		<div class="hint">Please enter the letters as they are shown in the image above.
-		<br/>Letters are not case-sensitive.</div>
-		<?php echo $form->error($model,'verifyCode'); ?>
-	</div>
-	<?php endif; ?>
+	<?php //if(CCaptcha::checkRequirements()): ?>
+	<!--<div class="row">-->
+		<?php //echo $form->labelEx($model,'verifyCode'); ?>
+		<!--<div>-->
+		<?php //$this->widget('CCaptcha'); ?>
+		<?php //echo $form->textField($model,'verifyCode',array('required'=>true)); ?>
+		<!--</div>-->
+		<?php //echo $form->error($model,'verifyCode'); ?>
+	<!--</div>-->
+	<?php //endif; ?>
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton('Submit'); ?>
+          
+		<?php echo CHtml::submitButton('Submit',array(
+                    'id'=>'contact_submit',
+                    'class'=>'submit'
+                            )); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
