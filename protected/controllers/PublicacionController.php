62e4fc02b2f4a3f21e16c16c4e274f9d2883c557
CristianCamilo <CristianCamilo@172.32.34.93>
2014-05-15 18:33:14 -0500

    Comentarios asociados a un post

--- protected/controllers/PublicacionController.php
+++ protected/controllers/PublicacionController.php
@@ -100,6 +100,7 @@ class PublicacionController extends Controller
 		{
 			$model->attributes=$_POST['Publicacion'];
                         $model->fecha= date("Y-m-d H:i:s");
+                        
 			if($model->save())
 				$this->redirect(array('view','id'=>$model->id));
 		}
