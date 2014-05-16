7d2045ea98a4c2aeaff243a90fa4d816f29cc602
jhta <jeisonhs93@gmail.com>
2014-05-15 23:30:04 -0500

    usuario index listo

--- protected/views/usuario/viewIndex.php
+++ protected/views/usuario/viewIndex.php
@@ -7,13 +7,13 @@
         	<div class="image-wrap img-rounded">
                 <div class="img-rounded hover-wrap">
                     <span class="overlay-img"></span>
-                    <span class="overlay-text-thumb"><?php echo $i['titulo']?></span>
+                    <span class="overlay-text-thumb"><?php echo $i['titulo'];?></span>
                 </div>
                 <img src="http://media.comicbookmovie.com/images/users/uploads/8073/Game_of_Thrones_Ygritte.jpg" alt="John Doe">
             </div>
-            <h3 class="profile-name"><?php $i['titulo']?></h3>
+            <h3 class="profile-name"><?php $i['titulo'];?></h3>
             <p class="profile-description">
-                <?php substr($i['descripcion'],0,305)."..."?>
+                <?php substr($i['descripcion'],0,305)."...";?>
             <div class="social">
             	<ul class="social-icons">
                 	<li><a href="#"><i class="font-icon-social-twitter"></i></a></li>
