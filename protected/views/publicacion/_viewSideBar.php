95d9a38df70d7762ee7064f532869cb1e0bdc3b0
jhta <jeisonhs93@gmail.com>
2014-05-18 16:19:16 -0500

    links en la derecha de post

--- protected/views/publicacion/_viewSideBar.php
+++ protected/views/publicacion/_viewSideBar.php
@@ -10,7 +10,7 @@
 
 ?>
 
-<li><a href="<?php echo CHtml::link(CHtml::encode($data->titulo), array('view', 'id'=>$data->id)); ?>">
+<li><a href="#<?php //echo CHtml::link(CHtml::encode($data->titulo), array('view', 'id'=>$data->id)); ?>">
         <?php echo CHtml::encode($data->titulo); ?>
     </a>
 </li>
