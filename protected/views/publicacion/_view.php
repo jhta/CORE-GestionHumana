4a52450b165dc6d9676a70ff768e3d4d9b3ea5f8
jhta <jeisonhs93@gmail.com>
2014-05-18 17:32:08 -0500

    labels

--- protected/views/publicacion/_view.php
+++ protected/views/publicacion/_view.php
@@ -10,6 +10,8 @@
     <div class="col-sm-10">
         <h3><?php echo CHtml::encode($data->titulo); ?></h3>
         <h4><span class="label label-default"><?php echo implode(' ', $data->tagLinks); ?></span></h4>
+        
+        <span class="text-muted" ><?php echo substr($string, CHtml::encode($data->contenido),0,150)."..."; ?></span>
         <h4> 
             <small class="text-muted">
                 <?php echo CHtml::encode($data->fecha); ?><br />
