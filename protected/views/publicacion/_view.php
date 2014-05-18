00cb42fe22c91b8342782c17d20cb9be61293236
jhta <jeisonhs93@gmail.com>
2014-05-18 18:01:33 -0500

    paginacion

--- protected/views/publicacion/_view.php
+++ protected/views/publicacion/_view.php
@@ -17,7 +17,7 @@
                 <label class="label label-tag"><?php echo CHtml::link('Leer Más', array('view', 'id'=>$data->id));?></label>
                 <label class="label label-tag"><?php echo CHtml::link("Comentarios ({$data->cuentaComentarios})",$data->url.'#comentarios'); ?></label>
             </small>
-            <smal class="text-muted">Publicado: <?php echo CHtml::encode($data->fecha); ?></smal>
+            <small class="text-muted">Publicado: <?php echo CHtml::encode($data->fecha); ?></small>
                 
             
         </h4>
