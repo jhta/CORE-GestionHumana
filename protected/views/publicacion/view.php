d6e46be3f3e180c82eb9c271f839598fbd3c6b8a
jhta <jeisonhs93@gmail.com>
2014-05-18 20:39:59 -0500

    vista publicacion (individual)

--- protected/views/publicacion/view.php
+++ protected/views/publicacion/view.php
@@ -16,33 +16,93 @@ $this->menu=array(
 );
 ?>
 
-
-<?php $this->renderPartial('_view2', array(
-	'data'=>$model,
-)); ?>
-
-<p></p>
-<div id="comentarios">
-    <?php if($model->cuentaComentarios>=1): ?>
-            <h3>
-                <?php echo $model->cuentaComentarios > 1 ? $model->cuentaComentarios . ' comentarios' : 'Un comentario'; ?>
-            </h3>
-
-            <?php $this->renderPartial('_comentarios',array(
-                    'post'=>$model,
-                    'comentarios'=>$model->comentarios,
-            )); ?>
-    <?php endif; ?>
-    
-    <h3>Dejanos un comentario</h3>
-
-    <?php if(Yii::app()->user->hasFlash('commentSubmitted')): ?>
-            <div class="flash-success">
-                    <?php echo Yii::app()->user->getFlash('commentSubmitted'); ?>
-            </div>
-    <?php else: ?>
-            <?php $this->renderPartial('/comentario/_form',array(
-                    'model'=>$comment,
-            )); ?>
-    <?php endif; ?>
+<!-- sidebar -->
+<div class="column col-xs-3" id="sidebar">
+    <a class="logo col-xs-1" href="#" style="margin-right: 0;">C</a>
+    <a class="logo col-xs-4 col-xs-offset-1" href="#" style="
+    margin-left: 0;
+    background: transparent;
+    color: white;
+">CORE</a>
+    <legend style="color: white;">Publicaciones</legend>
+    <ul class="nav">
+        
+        <?php  $this->widget('zii.widgets.CListView', array(
+                    'dataProvider'=>$dataProvider,
+                    'itemView'=>'_viewSideBar',
+                    'summaryText' => 'Se encontraron '. $pages->itemCount .' ',
+            ));
+//            $this->renderPartial('_viewSideBar', array(
+//            'publicacionesl' => $publicacionesl,
+//            ));
+        ?>
+    </ul>
+    <ul class="nav hidden-xs" id="sidebar-footer">
+        <li>
+            <a href="http://www.bootply.com"><h3>SINSO S.A</h3></a>
+        </li>
+    </ul>
 </div>
+<!-- /sidebar -->
+
+<!-- main -->
+<div class="column col-xs-9" id="main">
+    <div class="padding">
+        <div class="full col-sm-9">
+
+            <!-- content -->
+            <!--Titulo para post-->
+            <div class="col-sm-12" id="featured">   
+                <div class="page-header text-muted">
+                    Publicaciones
+                </div> 
+            </div>
+            <!--Fin Titulo para post-->
+<!--            ____________________________________________________________________________________
+            ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+                    CONTENIDO POST
+            ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-->
+            <?php $this->renderPartial('_view2', array(
+                    'data'=>$model,
+            )); ?>
+
+            <p></p>
+            <div id="comentarios">
+                <?php if($model->cuentaComentarios>=1): ?>
+                        <h3>
+                            <?php echo $model->cuentaComentarios > 1 ? $model->cuentaComentarios . ' comentarios' : 'Un comentario'; ?>
+                        </h3>
+
+                        <?php $this->renderPartial('_comentarios',array(
+                                'post'=>$model,
+                                'comentarios'=>$model->comentarios,
+                        )); ?>
+                <?php endif; ?>
+
+                <h3>Dejanos un comentario</h3>
+
+                <?php if(Yii::app()->user->hasFlash('commentSubmitted')): ?>
+                        <div class="flash-success">
+                                <?php echo Yii::app()->user->getFlash('commentSubmitted'); ?>
+                        </div>
+                <?php else: ?>
+                        <?php $this->renderPartial('/comentario/_form',array(
+                                'model'=>$comment,
+                        )); ?>
+                <?php endif; ?>
+            </div>
+
+            <!--Element-->
+               <!--footer-->
+               <?php $this->renderPartial('//site/footer');?>
+             <!--end Footer-->
+
+
+        </div><!-- /col-9 -->
+    </div><!-- /padding -->
+</div>
+<!-- /main -->
+
+
+
+
