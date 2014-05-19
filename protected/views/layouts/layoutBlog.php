ccdcb39cf39da58ed7fab5acebe597d8eb3ce41b
jhta <jeisonhs93@gmail.com>
2014-05-18 20:34:52 -0500

    paginacion

--- protected/views/layouts/layoutBlog.php
+++ protected/views/layouts/layoutBlog.php
@@ -17,6 +17,8 @@
 <!-- Optional theme -->
 <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 
+<!--font-->
+<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
 <!-- Latest compiled and minified JavaScript -->
 <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 
---
+++ protected/views/site/footer.php
@@ -0,0 +1,25 @@
+<!--Contacto-->
+<div class="col-sm-12">
+    <div class="page-header text-muted divider">
+        Conectate con nosotros
+    </div>
+</div>
+<!--Social-->
+<div class="row">
+    <div class="col-sm-6">
+        <a href="#">Twitter</a> <small class="text-muted">|</small> <a href="#">Facebook</a> <small class="text-muted">|</small> <a href="#">Google+</a>
+    </div>
+</div>
+<!--End Social-->
+<hr>
+<!--FOoter-->
+<div class="row" id="footer">    
+    <div class="col-sm-6">
+
+    </div>
+    <div class="col-sm-6">
+        <p>
+            <a href="#" class="pull-right">©Copyright Inc.</a>
+        </p>
+    </div>
+</div>
