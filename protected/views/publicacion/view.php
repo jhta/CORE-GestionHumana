53a11b83e413aa5e25d9c5a44b4e3bc70eb90069
jhta <jeisonhs93@gmail.com>
2014-05-18 20:54:01 -0500

    vista publicacion (individual)

--- protected/views/publicacion/view.php
+++ protected/views/publicacion/view.php
@@ -2,18 +2,18 @@
 /* @var $this PublicacionController */
 /* @var $model Publicacion */
 
-$this->breadcrumbs=array(
-	'Publicacions'=>array('index'),
-	$model->titulo,
-);
-$this->pageTitle=$model->titulo;
-$this->menu=array(
-	array('label'=>'List Publicacion', 'url'=>array('index')),
-	array('label'=>'Create Publicacion', 'url'=>array('create')),
-	array('label'=>'Update Publicacion', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Publicacion', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Publicacion', 'url'=>array('admin')),
-);
+//$this->breadcrumbs=array(
+//	'Publicacions'=>array('index'),
+//	$model->titulo,
+//);
+//$this->pageTitle=$model->titulo;
+//$this->menu=array(
+//	array('label'=>'List Publicacion', 'url'=>array('index')),
+//	array('label'=>'Create Publicacion', 'url'=>array('create')),
+//	array('label'=>'Update Publicacion', 'url'=>array('update', 'id'=>$model->id)),
+//	array('label'=>'Delete Publicacion', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+//	array('label'=>'Manage Publicacion', 'url'=>array('admin')),
+//);
 ?>
 
 <!-- sidebar -->
