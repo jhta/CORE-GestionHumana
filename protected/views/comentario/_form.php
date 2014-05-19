01d4c0c65b561b9a593ff4660c20bf584944e7ea
CristianCamilo <CristianCamilo@Camilo-PC>
2014-05-18 21:50:25 -0500

    comenzando a implementar popline

--- protected/views/comentario/_form.php
+++ protected/views/comentario/_form.php
@@ -15,7 +15,7 @@
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+	<p class="note">Requeridos <span class="required">*</span> </p>
 
 	<?php echo $form->errorSummary($model); ?>
 
