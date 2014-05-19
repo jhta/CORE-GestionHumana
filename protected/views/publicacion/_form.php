c68bc50dd4d63583b562ebcd5266e16b52c67db8
jhta <jeisonhs93@gmail.com>
2014-05-19 00:51:40 -0500

    comentarios en publicacion

--- protected/views/publicacion/_form.php
+++ protected/views/publicacion/_form.php
@@ -15,19 +15,29 @@
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
-
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'titulo'); ?>
-		<?php echo $form->textField($model,'titulo',array('size'=>50,'maxlength'=>50)); ?>
+		<?php //echo $form->labelEx($model,'titulo'); ?>
+		<?php echo $form->textField($model,'titulo',
+                        array('size'=>50,
+                            'maxlength'=>50,
+                            
+                            'class'=>'form-control',
+                            'placeholder'=>'nombre',
+                            
+                            )); ?>
 		<?php echo $form->error($model,'titulo'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'contenido'); ?>
-		<?php echo $form->textArea($model,'contenido',array('rows'=>50, 'cols'=>50)); ?>
+		<?php //echo $form->labelEx($model,'contenido'); ?>
+		<?php echo $form->textArea($model,'contenido',
+                        array('rows'=>50,
+                            'cols'=>50,
+                            'class'=>'form-control',
+                            'placeholder'=>'Escribe aqui tu comentario'
+                            )); ?>
 		<?php echo $form->error($model,'contenido'); ?>
 	</div>
 
@@ -38,7 +48,9 @@
 	</div>
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Comentar' : 'Enviar' , array(
+                    'class'=>'btn',
+                ) ); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
