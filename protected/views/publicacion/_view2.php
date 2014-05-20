3292d623d2931375be9217745ecc98ba629ab0ee
jhta <jeisonhs93@gmail.com>
2014-05-20 00:21:22 -0500

    nombre en post

--- protected/views/publicacion/_view2.php
+++ protected/views/publicacion/_view2.php
@@ -1,7 +1,8 @@
 <div class="post " >
-    <small class="author text-muted text-left">
-        publicado por <?php echo $this-> traerNombre($data->USUARIO_id). ' en ' . date('F j, Y H:i:s ', strtotime($data->fecha)); ?>
-    </small>
+    <small class="author text-muted ">
+        publicado por <?php echo $this-> traerNombre($data->USUARIO_id); ?>
+    </small><br>
+    <small class="text-muted"><?php echo  date('F j, Y H:i:s ', strtotime($data->fecha))?></small>
 
     <div class="title">
         <legend><?php echo CHtml::encode($data->titulo); ?></legend>
