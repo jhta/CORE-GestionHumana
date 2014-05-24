e270abcde4284913f6f1fda6092a90efd69f210b
CristianCamilo <CristianCamilo@Camilo-PC>
2014-05-24 00:37:07 -0500

    Creando panel de herramientas de edición

--- protected/views/publicacion/_form.php
+++ protected/views/publicacion/_form.php
@@ -4,6 +4,7 @@
 /* @var $form CActiveForm */
 ?>
 <head>
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/font-awesome.css" rel="stylesheet">
     <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/font-awesome.min.css" rel="stylesheet">
     <link href="<?php echo Yii::app()->request->baseUrl; ?>/popline/themes/popclip.css" rel="stylesheet">
     <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/popline/scripts/jquery.min.js"></script>
