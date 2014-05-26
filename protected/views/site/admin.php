ccfea4dd2fc694eb6cc4376626f1d628a4363afb
jhta <jeisonhs93@gmail.com>
2014-05-25 23:55:07 -0500

    vista admin

--- protected/views/site/admin.php
+++ protected/views/site/admin.php
@@ -1,27 +1,14 @@
-<!-- Modal -->
+<!-- Modal Para editar perfil-->
 <?php $this->renderPartial('//usuario/_edit'/*, array(
      'model' => $model,
     )*/);?>
-<div class="col-xs-3" id="slide-left">
-        <div id="edit-perfil">
-                <div class="imagen">
-                        <img  class="img-circle" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120">
-                </div>
-                <h3 class="nombre">jeison higuita sanchez</h3>
-                <div class="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-        </div>
-    <div class="btn btn-primary " data-toggle="modal" data-target="#myModal">
-  <span class="glyphicon glyphicon-edit"> </span> 
-  <span> Editar</span>
-</div>
-    
-    <!--<div class="btn btn-default"><span class="glyphicon glyphicon-edit"></span> Editar Perfil</div>-->
-</div>
+
+<!-- Informacion de perfil en columna de la izquierda-->
+<?php $this->renderPartial('//usuario/_viewAdmin'/*, array(
+     'model' => $model,
+    )*/);?>
+
+
 <div class="col-xs-9" id="slide-right">
         <nav class="navbar navbar-inverse " role="navigation">
                 <div class="navbar-right ">
