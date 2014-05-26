8305c3897d2788e7dc893f9bd04edce2ea23ab77
jhta <jeisonhs93@gmail.com>
2014-05-26 02:11:52 -0500

    login

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -226,7 +226,10 @@ class SiteController extends Controller
 			$model->attributes=$_POST['LoginForm'];
 			// validate user input and redirect to the previous page if valid
 			if($model->validate() && $model->login())
-				$this->redirect(Yii::app()->user->returnUrl);
+                            {
+                                $this->redirect(array('admin'));
+                                
+                            }
 		}
 		// display the login form
 		$this->render('login',array('model'=>$model));
