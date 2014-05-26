38a5aff7d162ac1fac5d3489442d3a08fa91a5a2
jhta <jeisonhs93@gmail.com>
2014-05-26 00:42:28 -0500

    vista admin

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -29,7 +29,10 @@ class UserIdentity extends CUserIdentity
 			$this->_id= $user->id;
                         $this->setState('nombre',$user->nombre);
 			$this->setState('username',$user->username);
-                        
+                        $this->setState('descripcion', $user->descripcion);
+                        $this->setState('nombre_foto', $user->nombre_foto);
+                        $this->setState('formato_foto', $user->formato_foto);
+                        $this->setState('titulo', $user->titulo);
                         $this->username= $user->username;
 			$this->errorCode= self::ERROR_NONE;
 		}
