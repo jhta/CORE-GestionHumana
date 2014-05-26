7235e05e4878c2b39131d29a53d1483a50725bbd
jhta <jeisonhs93@gmail.com>
2014-05-26 02:15:19 -0500

    login

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -28,12 +28,8 @@ class UserIdentity extends CUserIdentity
 		{
 			$this->_id= $user->id;
                         $this->setState('nombre',$user->nombre);
-			$this->_descripcion= $user->descripcion;
+			
                         $this->setState('username',$user->username);
-                        $this->setState('descripcion', $user->descripcion);
-                        $this->setState('nombre_foto', $user->nombre_foto);
-                        $this->setState('formato_foto', $user->formato_foto);
-                        $this->setState('titulo', $user->titulo);
                         $this->username= $user->username;
 			$this->errorCode= self::ERROR_NONE;
 		}
