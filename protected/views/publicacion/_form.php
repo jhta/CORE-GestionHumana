62b0249aef3c178aba9307b3a1875ca919341186
CristianCamilo <CristianCamilo@Camilo-PC>
2014-05-26 23:13:22 -0500

    Edición de post

--- protected/views/publicacion/_form.php
+++ protected/views/publicacion/_form.php
@@ -29,10 +29,6 @@ $(document).ready(function(){
 
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'publicacion-form',
-	// Please note: When you enable ajax validation, make sure the corresponding
-	// controller action is handling ajax validation correctly.
-	// There is a call to performAjaxValidation() commented in generated controller code.
-	// See class documentation of CActiveForm for details on this.
 	'enableAjaxValidation'=>false,
 )); ?>
 
