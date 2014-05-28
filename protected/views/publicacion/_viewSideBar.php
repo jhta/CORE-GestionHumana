0b29ad3e4d21c81991256d297148af9de145e8cc
jhta <jeisonhs93@gmail.com>
2014-05-27 23:01:58 -0500

    botn de nueva publicacion

--- protected/views/publicacion/_viewSideBar.php
+++ protected/views/publicacion/_viewSideBar.php
@@ -12,7 +12,7 @@
 
 <li class="sideBar-link">
     <span class="glyphicon glyphicon-eye-open"></span>
-    <?php echo substr(CHtml::link(CHtml::encode($data->titulo), array('view', 'id'=>$data->id)),0,30); ?>
+    <?php echo substr(CHtml::link(CHtml::encode($data->titulo), array('view', 'id'=>$data->id)),0,60); ?>
     <span class="label label-default pull-right" style="
     background: #cccc99;
 "><?php echo substr(CHtml::encode($data->fecha),0,10) ; ?></span>
