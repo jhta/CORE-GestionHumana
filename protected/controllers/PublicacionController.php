5452f16f3628adc63e8127b3d063e88b6d4efc37
CristianCamilo <CristianCamilo@172.32.20.35>
2014-05-28 08:34:06 -0500

    Redirección de creación de post

--- protected/controllers/PublicacionController.php
+++ protected/controllers/PublicacionController.php
@@ -164,7 +164,7 @@ class PublicacionController extends Controller
                                 }
                             }
                                 
-                            $this->redirect(Yii::app()->createAbsoluteUrl(array('publicacion/view','id'=>$model->id)));
+                            $this->redirect(array(Yii::app()->createAbsoluteUrl('publicacion/view'),'id'=>$model->id));
                         }
                 }
         }
