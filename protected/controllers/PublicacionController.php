7bc13bbb6546f7b71bcb638c60375a716f73d34a
jhta <jhta@jhta-Aspire-S3-391>
2014-05-28 17:52:40 -0500

    boton Eliminrar

--- protected/controllers/PublicacionController.php
+++ protected/controllers/PublicacionController.php
@@ -32,11 +32,11 @@ class PublicacionController extends Controller
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update','crear','actualizar'),
+				'actions'=>array('create','update','crear','actualizar','delete'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
+				'actions'=>array('admin'),
 				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
