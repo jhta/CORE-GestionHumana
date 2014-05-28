7e6be51e0b1ba8323759bc91c22c7255d75550e2
CristianCamilo <CristianCamilo@172.32.20.35>
2014-05-28 09:31:57 -0500

    Conteo de clicks

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -49,7 +49,7 @@ class SiteController extends Controller
 	public function actionIndex(){
             $modelI= Informacion::model()->findByPk(1);
              
-            $num= $modelI->total_clicks;
+            $num= $modelI->total_clicks + 1;
             echo $num;        
                 $Criteria = new CDbCriteria();
                 $Criteria->limit = 2;
