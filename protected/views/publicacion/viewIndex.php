5041d00a78ed7c184144a5c32d0ba90b13b497e3
jhta <jeisonhs93@gmail.com>
2014-05-28 01:47:37 -0500

    f

--- protected/views/publicacion/viewIndex.php
+++ protected/views/publicacion/viewIndex.php
@@ -1,7 +1,34 @@
 <div class="title-page">
                     <h2 class="title">Ultimos Post</h2>
                </div>
-               
+                <div class="span6">
+                  
+                    
+                    <!-- Start Accordion -->
+                    <div class="accordion" id="toggleArea">
+                        <?php foreach ($Publicaciones2 as $i) {
+    
+                        ?>
+                        <div class="accordion-group">
+                            <div class="accordion-heading accordionize">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionArea" href="#oneArea">
+                                    <?php echo $i['titulo']?>
+                                    <span class="font-icon-arrow-simple-down"></span>
+                                </a>
+                            </div>
+                            <div id="oneArea" class="accordion-body collapse">
+                                <div class="accordion-inner">
+                                    <?php echo mb_substr($i['contenido'], 0,300); ?>
+                                
+                                </div>
+                            </div>
+                        </div>
+                        <?php }?>
+                        
+                    </div>
+                    <!-- End Accordion -->
+                </div>
+                 
                 <div class="span6">
                   
                     
@@ -30,31 +57,4 @@
                     </div>
                     <!-- End Accordion -->
                 </div>
-                 <div class="span6">
-                  
                     
-                    <!-- Start Accordion -->
-                    <div class="accordion" id="toggleArea">
-                        <?php foreach ($Publicaciones2 as $i) {
-    
-                        ?>
-                        <div class="accordion-group">
-                            <div class="accordion-heading accordionize">
-                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionArea" href="#oneArea">
-                                    <?php echo $i['titulo']?>
-                                    <span class="font-icon-arrow-simple-down"></span>
-                                </a>
-                            </div>
-                            <div id="oneArea" class="accordion-body collapse">
-                                <div class="accordion-inner">
-                                    <?php echo mb_substr($i['contenido'], 0,300); ?>
-                                
-                                </div>
-                            </div>
-                        </div>
-                        <?php }?>
-                        
-                    </div>
-                    <!-- End Accordion -->
-                </div>
-                
