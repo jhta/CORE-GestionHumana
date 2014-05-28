6c2b64ffc03b6e9a7b0567acc19d23f281bab106
jhta <jeisonhs93@gmail.com>
2014-05-28 00:58:04 -0500

    sdfsd

--- protected/views/publicacion/_publicar.php
+++ protected/views/publicacion/_publicar.php
@@ -81,14 +81,14 @@ $(document).ready(function(){
                             'style'=>'display: none;',
                             )); ?>
 		<?php echo $form->error($model,'contenido'); ?>
-	</div>
+	
     
         <div class="editor row" contenteditable="true" id="contenido">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
         </div>
-
+        </div>
 	<div class="row form-group" >
             <h4 class="text-center col-xs-1 col-xs-offset-1">Autor</h4>
 		<div class="col-xs-9">
