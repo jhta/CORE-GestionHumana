2fecb30dbeab8934afbec761bf4785939880cc7b
jhta <jeisonhs93@gmail.com>
2014-05-28 01:49:22 -0500

    f

--- protected/views/publicacion/viewIndex.php
+++ protected/views/publicacion/viewIndex.php
@@ -5,7 +5,7 @@
                   
                     
                     <!-- Start Accordion -->
-                    <div class="accordion" id="toggleArea">
+                    <div class="accordion" id="accordionArea">
                         <?php foreach ($Publicaciones2 as $i) {
     
                         ?>
@@ -33,7 +33,7 @@
                   
                     
                     <!-- Start Accordion -->
-                    <div class="accordion" id="accordionArea">
+                    <div class="accordion" id="toogleArea">
                         <?php foreach ($Publicaciones as $i) {
     
                         ?>
