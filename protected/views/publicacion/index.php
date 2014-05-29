de8a291175a44fc5f706dcb08934523ad26fef1c
jhta <jhta@jhta-Aspire-S3-391>
2014-05-29 16:00:41 -0500

    errores menores

--- protected/views/publicacion/index.php
+++ protected/views/publicacion/index.php
@@ -47,16 +47,7 @@
                     <!--Barra de bisqueda-->
                     
                 </div>
-                <div class="row">
-                        <p class="col-xs-2">Buscar: </p>  
-                    <div class="input-group col-xs-4">
-                        
-                    <input type="search" class="form-control" placeholder="buscar Publicacion..">
-                    <span class="input-group-btn">
-                      <button class="btn btn-default" type="button">Buscar</button>
-                    </span>
-                  </div><!-- /input-group -->
-                    </div>
+                
             </div>
             <!--Fin Titulo para post-->
 <!--            ____________________________________________________________________________________
