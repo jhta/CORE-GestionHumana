e38b11015e105372baf46e15e4fa8fbc2e12dc41
jhta <jhta@jhta-Aspire-S3-391>
2014-05-29 00:25:47 -0500

    table publication's

--- protected/views/publicacion/_viewAdmin.php
+++ protected/views/publicacion/_viewAdmin.php
@@ -1,6 +1,6 @@
 
 <div class="row" id="tabla">
-                    <div class="col-xs-8 pad-0">
+                    <div class=" col-xs-offset-1 col-xs-8 pad-0">
                         <table class="table table-striped table-hover">
                             <trhead>
                                 <th><span class="glyphicon glyphicon-th-list"></span> Publicacion</th>
