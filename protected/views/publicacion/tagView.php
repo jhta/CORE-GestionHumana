3a2dc22f6131c1738f7e1372390832bf1a3a9df0
jhta <jhta@jhta-Aspire-S3-391>
2014-05-29 12:14:36 -0500

    table publication's

--- protected/views/publicacion/tagView.php
+++ protected/views/publicacion/tagView.php
@@ -1,4 +1,4 @@
-<div class="column col-xs-3" id="sidebar">
+<div class="column col-sm-3 " id="sidebar">
     <a class="logo col-xs-1" href="#" style="margin-right: 0;">C</a>
     <a class="logo col-xs-4 col-xs-offset-1" href="#" style="
     margin-left: 0;
@@ -28,7 +28,7 @@
 <!-- /sidebar -->
 
 <!-- main -->
-<div class="column2 col-xs-9" id="main">
+<div class="column2 col-sm-9 col-xs-12" id="main">
     <div class="padding">
         <div class="full col-sm-9">
 
