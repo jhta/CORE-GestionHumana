df663bcd25894b9fa03db27380651ae84e191498
jhta <jhta@jhta-Aspire-S3-391>
2014-05-29 01:39:49 -0500

    table publication's

--- protected/views/publicacion/_view.php
+++ protected/views/publicacion/_view.php
@@ -30,11 +30,7 @@
    
     font-size: 12px;
     margin-top: 10px;
-">por:</p> </br><p class="text-muted text-center" style="
-   
-    font-size: 12px;
-    margin-top: 10px;
-">sfdffgffdgf</p>
+">por: sfdffgffdgf</p>
 </div>
 <div class="row divider">    
     <div class="col-sm-12"><hr></div>
