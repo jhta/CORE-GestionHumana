c779c63e26141039a788f0c8f4ae8ebd7bf451fa
CristianCamilo <CristianCamilo@Camilo-PC>
2014-05-29 00:18:27 -0500

    Gráficas por fin

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -236,7 +236,7 @@ class SiteController extends Controller
             
             foreach($analytic as $dato){
                 $mes[]= $dato->month;
-                $visita[]= $dato->visitas;
+                $visita[]= (int) $dato->visitas;
             }
             $jsonMes= json_encode($mes);
             $jsonVis= json_encode($visita);
