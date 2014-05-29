3b170381d4bd5391b884a7b35f9c57e56c43ad68
jhta <jhta@jhta-Aspire-S3-391>
2014-05-29 13:09:11 -0500

    table publication's

--- protected/views/publicacion/_view.php
+++ protected/views/publicacion/_view.php
@@ -30,7 +30,7 @@
    
     font-size: 12px;
     margin-top: 10px;
-    ">por: <?php echo Usuario::model()->findByPk($data->USUARIO_id)->titulo;?></p>
+    ">por: <?php echo Usuario::model()->findByPk($data->USUARIO_id)->nombre;?></p>
 </div>
 <div class="row divider">    
     <div class="col-sm-12"><hr></div>
